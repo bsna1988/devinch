320fe90616d5cd71b82b597e1ba8bed5724efbac
Sasha Bogolii <bsna1988@gmail.com>
2022-05-04 14:40:22 +0300

    Split into components index.html

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react';
-import Header from './components/Header/Header';
-import './App.scss';
+import React, { Component } from "react";
+import Header from "./components/Header/Header";
+import WhatsNew from "./components/WhatsNew/WhatsNew";
+import Footer from "./components/Footer/Footer";
+import "./App.scss";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />    
+        <Header />
+        <WhatsNew />
+        <Footer />
       </div>
     );
   }
---
+++ frontend/src/components/Footer/Footer.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Footer.scss";
+const Header = () => (
+  <footer>
+    <div class="footer-bottom-area">
+      <div class="container">
+        <div class="footer-border">
+          <div class="row d-flex align-items-center justify-content-between">
+            <div class="col-lg-6">
+              <div class="footer-copy-right">
+                <p>
+                  Copyright &copy;
+                  <script>document.write(new Date().getFullYear());</script>
+                  All rights reserved | This template is made with
+                  <i class="ti-heart" aria-hidden="true"></i> by
+                  <a href="https://colorlib.com" target="_blank">
+                    Colorlib
+                  </a>
+                </p>
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <div class="footer-menu f-right">
+                <ul>
+                  <li>
+                    <a href="/contact.html">Contact</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+);
+export default Header;
