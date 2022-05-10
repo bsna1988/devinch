326736a7b604ef14bb77f62bc911ebf87fcfac1e
Sasha Bogolii <bsna1988@gmail.com>
2022-05-10 14:41:41 +0300

    Removed categories on WhatsNew

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -9,7 +9,7 @@ class Home extends Component {
   render() {
     return (
       <div className="App">
-         <Preloader/>
+        <Preloader />
         <Header />
         <WhatsNew />
         <Footer />
