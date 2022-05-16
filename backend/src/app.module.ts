6834afe9c7be8b51c111e5a3e796fd7c37e5e27e
Sasha Bogolii <bsna1988@gmail.com>
2022-05-16 13:35:18 +0300

    Removed complicated logic used for configuration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,14 +3,24 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoryModule } from './category/category.module';
 import { PostModule } from './post/post.module';
 import { TopicModule } from './topic/topic.module';
-import { configService } from './config/config.service';
+import { Post } from './post/entities/post.entity';
+require('dotenv').config();
 
 @Module({
   imports: [
     TopicModule,
     PostModule,
     CategoryModule,
-    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.devinch,
+      entities: [Post],
+      synchronize: true,
+    }),
   ],
 })
 export class AppModule {}
