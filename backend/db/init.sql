041c6448ea3fe334a1b0aea5cc66a1fe275f8d9a
Sasha Bogolii <bsna1988@gmail.com>
2022-05-23 15:24:57 +0300

    Added post_content table

--- backend/db/init.sql
+++ backend/db/init.sql
@@ -1,6 +1,20 @@
-create table Post (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    title text not null,
+CREATE TABLE IF NOT EXISTS public.post
+(
+    id uuid NOT NULL DEFAULT gen_random_uuid(),
+    title text,
     description text,
-    author text
-)
+    author text,
+    created timestamp DEFAULT now(),
+    content_id uuid,
+    CONSTRAINT post_pkey PRIMARY KEY (id),
+    CONSTRAINT post_content_id FOREIGN KEY (content_id)
+        REFERENCES public.post_content (id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS public.post_content
+(
+    id uuid NOT NULL DEFAULT gen_random_uuid(),
+    content text,
+    CONSTRAINT post_content_pkey PRIMARY KEY (id)
+);
