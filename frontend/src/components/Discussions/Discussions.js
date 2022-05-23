13b8e012db4b03cef3583dddb3962d3b3f89ca8c
Sasha Bogolii <bsna1988@gmail.com>
2022-05-23 20:16:57 +0300

    Fixed routing

--- frontend/src/components/Discussions/Discussions.js
+++ frontend/src/components/Discussions/Discussions.js
@@ -70,7 +70,7 @@ export default class Discussions extends React.Component {
                   return (
                     <article class="blog_item" key={e.id}>
                       <div class="blog_details">
-                        <a class="d-inline-block" href={`/post/${e.id}`}>
+                        <a class="d-inline-block" href={`/discussions/${e.id}`}>
                           <h2>{e.title}</h2>
                         </a>
                         <p>{e.description}</p>
